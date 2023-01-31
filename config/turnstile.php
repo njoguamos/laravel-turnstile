8df472375c33b524580b1ac33ee90190fa9e6fd2
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-01-31 21:57:40 +0300

    Update turnstile.php

--- config/turnstile.php
+++ config/turnstile.php
@@ -14,5 +14,31 @@ return [
      |
      */
     'secretkey' => env(key: 'TURNSTILE_SECRET_KEY'),
-    'sitekey'   => env(key: 'TURNSTILE_SITE_KEY')
+    'sitekey'   => env(key: 'TURNSTILE_SITE_KEY'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Status
+     |--------------------------------------------------------------------------
+     |
+     | This option allows you to enable or disable turnstile. This is useful
+     | when testing your application. Disabling ensure your tests runs
+     | smoothly. remember to enable once you are done testing.
+     |
+     |
+     */
+    'enabled'   => env(key: 'TURNSTILE_ENABLED', default: true),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Field name
+     |--------------------------------------------------------------------------
+     |
+     | This is the name of the turnstile validation field from your front-end
+     | application. Failure to include the field in the request will cause
+     | a runtime exception. This is precautionary measure.
+     |
+     |
+     */
+    'field'     => env(key: 'TURNSTILE_FIELD', default: 'cf-turnstile-response'),
 ];
