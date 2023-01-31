7741e06e0a33edbd1336ba781bb38ac589b94540
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-01-31 23:13:17 +0300

    Update TurnstileTest.php

--- tests/TurnstileTest.php
+++ tests/TurnstileTest.php
@@ -9,11 +9,26 @@ it(description: 'throw an exception when secret key is not available', closure: 
     $this->app->make(abstract: Turnstile::class);
 })->throws(exception: RuntimeException::class);
 
-it(description: 'it return correct response based on provided key', closure: function (string $secret, bool $expected) {
-    config()->set(key: 'turnstile.secretkey', value: $secret);
+it(description: 'it return true when valid token is provided', closure: function () {
+    setSecretKey(type: 'valid');
 
     $turnstile = $this->app->make(abstract: Turnstile::class);
 
-    expect(value: $turnstile->validate(Str::random(length: 100)))
-        ->toBe($expected);
-})->with('tokens-dataset');
+    expect(value: $turnstile->validate(Str::random(length: 100)))->toBeTrue();
+});
+
+it(description: 'it return false when invalid token is provided', closure: function () {
+    setSecretKey(type: 'invalid');
+
+    $turnstile = $this->app->make(abstract: Turnstile::class);
+
+    expect(value: $turnstile->validate(Str::random(length: 100)))->toBeFalse();
+});
+
+it(description: 'it return false when used token is provided', closure: function () {
+    setSecretKey(type: 'spent');
+
+    $turnstile = $this->app->make(abstract: Turnstile::class);
+
+    expect(value: $turnstile->validate(Str::random(length: 100)))->toBeFalse();
+});
