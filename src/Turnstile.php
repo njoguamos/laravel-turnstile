5cde2e62331796c0e2ae9518061b3dc29227bcd1
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2023-01-31 17:54:51 +0300

    Update Turnstile.php

--- src/Turnstile.php
+++ src/Turnstile.php
@@ -7,27 +7,19 @@ use RuntimeException;
 
 class Turnstile
 {
-    protected string $url;
-
-    protected string $secretkey;
-
-    public function __construct()
+    public function __construct(protected string $url, protected ?string $secretKey)
     {
-        $this->url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-
-        $this->secretkey = config(key: 'turnstile.secretkey');
-
-        if (empty($this->secretkey)) {
+        if (empty($this->secretKey)) {
             throw new RuntimeException(message:  trans(key: 'turnstile::turnstile.no_secret_key'));
         }
     }
 
-    public function validate(string $response): bool
+    public function validate(string $token): bool
     {
         $response = Http::acceptJson()
             ->post($this->url, [
-            'response' => $response,
-            'secret'   => $this->secretkey
+            'response' => $token,
+            'secret'   => $this->secretKey
         ]);
 
         return (bool) $response->json()['success'];
