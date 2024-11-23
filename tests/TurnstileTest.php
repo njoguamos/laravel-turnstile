493838a6bf27220fed00c3699ebb2d31c6b9977e
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-11-23 22:32:43 +0300

    update pest-plugin-laravel

--- tests/TurnstileTest.php
+++ tests/TurnstileTest.php
@@ -9,20 +9,20 @@ it(description: 'throw an exception when secret key is not available', closure: 
     new Turnstile();
 })->throws(exception: RuntimeException::class);
 
-it(description: 'it return true when valid token is provided', closure: function () {
+it(description: 'return true when valid token is provided', closure: function () {
     setSecretKey(type: 'valid');
 
     expect(value: (new Turnstile())->validate(Str::random(length: 100)))->toBeTrue();
 });
 
-it(description: 'it return false when invalid token is provided', closure: function () {
+it(description: 'return false when invalid token is provided', closure: function () {
     setSecretKey(type: 'invalid');
 
 
     expect(value: (new Turnstile())->validate(Str::random(length: 100)))->toBeFalse();
 });
 
-it(description: 'it return false when used token is provided', closure: function () {
+it(description: 'return false when used token is provided', closure: function () {
     setSecretKey(type: 'spent');
 
     expect(value: (new Turnstile())->validate(Str::random(length: 100)))->toBeFalse();
