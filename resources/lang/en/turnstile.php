608f547471a0f8fdd5e1ebda250a61d30f43b80e
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-11-23 23:24:30 +0300

    Update failed validation message (#23)

--- resources/lang/en/turnstile.php
+++ resources/lang/en/turnstile.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'failed'        => "We couldn't verify if you're human. Please refresh the page and try again.",
+    'failed'        => "We couldn't verify if you're human. Please try again.",
     'no_secret_key' => 'Secret key was not found. Please add the key to your services file.',
 ];
