a0383ae6d439aef1997706d22f17bba5df4895f5
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-11-23 22:39:06 +0300

    update read me

--- src/Http/Middleware/TurnstileMiddleware.php
+++ src/Http/Middleware/TurnstileMiddleware.php
@@ -4,7 +4,6 @@ namespace NjoguAmos\Turnstile\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Container\BindingResolutionException;
-
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use NjoguAmos\Turnstile\Turnstile;
